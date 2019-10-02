830dfa7ffdcd83b719b0513ef40c36ce6e6dc579
gewyckler <gewyckler@gmail.com>
2019-10-02 18:00:08 +0200

    Inicialized commit

---
+++ src/main/java/pl/javagda25/spring/la_biblioteca/LaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package pl.javagda25.spring.la_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaBibliotecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javagda25/spring/la_biblioteca/LaBibliotecaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.javagda25.spring.la_biblioteca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaBibliotecaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
